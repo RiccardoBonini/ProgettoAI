5088811ff05023f61bf1f7fe23042b1a4d12f74d
RiccardoBonini <38753018+RiccardoBonini@users.noreply.github.com>
2020-06-11 17:39:50 +0200

    Add files via upload

--- Main.py
+++ Main.py
@@ -6,41 +6,41 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 #function to permute the training set
-def permute(preX, prey):
+def permute(preX, preY):
     supportmatrix = []
-    for i in range(len(prey)):
+    for i in range(len(preY)):
         support = []
         for j in range(len(preX[i])):
             support.append(preX[i][j])
-        support.append(prey[i])
+        support.append(preY[i])
         supportmatrix.append(support)
 
     np.random.shuffle(supportmatrix)
 
-    y_train = []
+    Y_train = []
     X_train = []
 
     for i in range(len(supportmatrix)):
-        y_train.append(np.array(supportmatrix[i][-1]))
+        Y_train.append(np.array(supportmatrix[i][-1]))
         X_train.append(np.array(supportmatrix[i][0:-1]))
 
-    return  X_train, y_train
+    return X_train, Y_train
 
 #we load the complete dataset
-X1, y1 = mnist_reader.load_mnist('data/fashion', kind='train')
-X2, y2 = mnist_reader.load_mnist('data/fashion', kind='t10k')
+X1, Y1 = mnist_reader.load_mnist('data/fashion', kind='train')
+X2, Y2 = mnist_reader.load_mnist('data/fashion', kind='t10k')
 
 #this for cycle is for the 2 example of binary classifications
 for run in range(2):
 
     X_train = []
-    y_train = []
+    Y_train = []
 
     X_test = []
-    y_test = []
+    Y_test = []
 
     preX = []
-    prey = []
+    preY = []
 
     #each time we select 2 different classes for the perceptron
     if run == 0:
@@ -51,27 +51,27 @@ for run in range(2):
         class2 = 9
 
     #we select the elements of the 2 chosen classes from the original dataset
-    for i in range(len(y1)):
-        if y1[i] == class1 or y1[i] == class2:
+    for i in range(len(Y1)):
+        if Y1[i] == class1 or Y1[i] == class2:
             preX.append(X1[i])
-            if y1[i] == class1:
-                prey.append(-1)
+            if Y1[i] == class1:
+                preY.append(-1)
             else:
-                prey.append(1)
+                preY.append(1)
 
-    for i in range(len(y2)):
-        if y2[i] == class1 or y2[i] == class2:
+    for i in range(len(Y2)):
+        if Y2[i] == class1 or Y2[i] == class2:
             X_test.append(X2[i])
-            if y2[i] == class1:
-                y_test.append(-1)
+            if Y2[i] == class1:
+                Y_test.append(-1)
             else:
-                y_test.append(1)
+                Y_test.append(1)
 
     #we normalize the values
     preX[:] = [x / 255 for x in preX]
     X_test[:] = [ x / 255 for x in X_test]
 
-    X_train, y_train = permute(preX, prey)
+    X_train, Y_train = permute(preX, preY)
 
     #creating 2 vector to plot for the Test Errors Graph
     plot_epochs = []
@@ -86,15 +86,15 @@ for run in range(2):
 
         print('Training the voted perceptron on classes: ', class1, 'and: ', class2, 'in dimension = ', dimension)
 
-        kernel_voted_perceptron.train(X_train, y_train)
+        kernel_voted_perceptron.train(X_train, Y_train)
 
         print('Training completed')
         errors.append(kernel_voted_perceptron.errors)
 
         predictions = kernel_voted_perceptron.vote(X_test)
 
-        print('Accuracy score of the prediction on test labels in dimension', dimension, ':', accuracy_score(y_test, predictions))
-        print('Classification report for prediction in dimension', dimension, ': \n\n', classification_report(y_test, predictions))
+        print('Accuracy score of the prediction on test labels in dimension', dimension, ':', accuracy_score(Y_test, predictions))
+        print('Classification report for prediction in dimension', dimension, ': \n\n', classification_report(Y_test, predictions))
         print()
 
     #plotting the errors-per-epoch function
