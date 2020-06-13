9f0151d6655c64e00bc601c2d5c3aee2565e1b73
RiccardoBonini <38753018+RiccardoBonini@users.noreply.github.com>
2020-06-13 12:23:51 +0200

    Add files via upload

--- KernelVotedPerceptron.py
+++ KernelVotedPerceptron.py
@@ -7,11 +7,11 @@ class KernelVotedPerceptron:
     def __init__(self, T, d):
         self.T = T
         self.C = []
-        self.K = 0
+        self.K = []
         self.WY = []
         self.WX = []
         self.d = d
-        self.errors = []
+
 
     def train(self, x, y):
 
@@ -19,7 +19,7 @@ class KernelVotedPerceptron:
         wy = []
         wx = []
         c = [0]
-        er = []
+        partial = []
         for epoch in range(self.T):
             for i in range(len(x)):
                 s = 0
@@ -27,7 +27,7 @@ class KernelVotedPerceptron:
                     s = 0
                 else:
                     for j in range(k):
-                        s += wy[j] * kernel(wx[j], x[i], d = self.d)
+                        s += wy[j] * kernel(wx[j], x[i], d=self.d)
                 pred = 1 if s > 0 else -1
                 if pred == y[i]:
                     c[k] += 1
@@ -36,32 +36,37 @@ class KernelVotedPerceptron:
                     wx.append(x[i])
                     c.append(1)
                     k += 1
-                if i % (len(x)/20) == 0: er.append(k)
+                if epoch != 0 or i != 0:          #every tenth of an epoch (exluding 0) we save the number of errors k so that we can recreate the partial wight vector
+                    if i % (len(x) / 10) == 0: partial.append(k)
 
         self.C = c
         self.K = k
         self.WY = wy
         self.WX = wx
-        self.errors = er
 
-    def vote(self, x):
+        return partial
+
+    def vote(self, x, partial):
         predictions = []
         x0 = np.zeros(len(x[0]))
+
         for l in range(len(x)):
 
             #for each x, we store the product vx as an array so we don't have to calculate it every cycle
             vx = []
             vx.append(0 + 1*kernel(x0, x[l], d= self.d)) #this is the first element, we treat the first mistaken x as a vector of all zeros
-            for j in range(1, self.K):
+            for j in range(1, partial[-1]):
                 vx.append(vx[j - 1] + self.WY[j] * kernel(self.WX[j], x[l], d = self.d))
 
-            s = 0
-            for i in range(self.K):
+            #we now compute a prediction using every partial weight vector
+            partial_predictions = []
+            for k in range (len(partial)):
+                s = 0
+                for i in range(partial[k]):
+                    s = s + self.C[i] * np.sign(vx[i])
+                partial_predictions.append(np.sign(s))
 
-                s = s + self.C[i] * np.sign(vx[i])
-
-            predictions.append(np.sign(s))
+            predictions.append(partial_predictions)
         return predictions
-
 
 
--- Main.py
+++ Main.py
@@ -1,109 +1,98 @@
+import numpy as np
 import mnist_reader
 import KernelVotedPerceptron as kvp
 from sklearn.metrics import accuracy_score
 from sklearn.metrics import classification_report
-import numpy as np
 import matplotlib.pyplot as plt
 
-#function to permute the training set
-def permute(preX, preY):
-    supportmatrix = []
-    for i in range(len(preY)):
-        support = []
-        for j in range(len(preX[i])):
-            support.append(preX[i][j])
-        support.append(preY[i])
-        supportmatrix.append(support)
-
-    np.random.shuffle(supportmatrix)
-
-    Y_train = []
-    X_train = []
-
-    for i in range(len(supportmatrix)):
-        Y_train.append(np.array(supportmatrix[i][-1]))
-        X_train.append(np.array(supportmatrix[i][0:-1]))
-
-    return X_train, Y_train
 
 #we load the complete dataset
-X1, Y1 = mnist_reader.load_mnist('data/fashion', kind='train')
-X2, Y2 = mnist_reader.load_mnist('data/fashion', kind='t10k')
+X1, y1 = mnist_reader.load_mnist('data/fashion', kind='train')
+X2, y2 = mnist_reader.load_mnist('data/fashion', kind='t10k')
 
-#this for cycle is for the 2 example of binary classifications
-for run in range(2):
 
-    X_train = []
-    Y_train = []
+X_train = []
+y_train = []
 
-    X_test = []
-    Y_test = []
+X_test = []
+y_test = []
 
-    preX = []
-    preY = []
+class1 = 1
+class2 = 8
 
-    #each time we select 2 different classes for the perceptron
-    if run == 0:
-        class1 = 0
-        class2 = 5
-    if run == 1:
-        class1 = 1
-        class2 = 9
+#we load the elements of the 2 classes in our train and test set
+for i in range(len(y1)):
+    if y1[i] == class1 or y1[i] == class2:
+        X_train.append(X1[i])
+        if y1[i] == class1:
+            y_train.append(-1)
+        else:
+            y_train.append(1)
 
-    #we select the elements of the 2 chosen classes from the original dataset
-    for i in range(len(Y1)):
-        if Y1[i] == class1 or Y1[i] == class2:
-            preX.append(X1[i])
-            if Y1[i] == class1:
-                preY.append(-1)
-            else:
-                preY.append(1)
+for i in range(len(y2)):
+    if y2[i] == class1 or y2[i] == class2:
+        X_test.append(X2[i])
+        if y2[i] == class1:
+            y_test.append(-1)
+        else:
+            y_test.append(1)
 
-    for i in range(len(Y2)):
-        if Y2[i] == class1 or Y2[i] == class2:
-            X_test.append(X2[i])
-            if Y2[i] == class1:
-                Y_test.append(-1)
-            else:
-                Y_test.append(1)
 
-    #we normalize the values
-    preX[:] = [x / 255 for x in preX]
-    X_test[:] = [ x / 255 for x in X_test]
+#normalize the data
+X_train[:] = [ x / 255 for x in X_train]
+X_test[:] = [ x / 255 for x in X_test]
 
-    X_train, Y_train = permute(preX, preY)
+#this vector will be used to plot both the test error and the learning curve
+plot_epochs = [i for i in np.arange(0.1,10,0.1)]
+#this is for the learning curve
+errors_during_training = []
 
-    #creating 2 vector to plot for the Test Errors Graph
-    plot_epochs = []
-    for i in range(200):
-        plot_epochs.append(i * 1 / 25)
+for dimension in range(1,4):     #we start from dimension = 1 up to dimension = 3
+
 
     errors = []
 
-    for dimension in range(1,4):     #we start from dimension = 1 up to dimension = 3
+    kernel_voted_perceptron = kvp.KernelVotedPerceptron(T=10, d = dimension)
 
-        kernel_voted_perceptron = kvp.KernelVotedPerceptron(T=10, d = dimension)
+    print('Training the voted perceptron in dimension = ', dimension, 'on classes ', class1, 'and ', class2)
 
-        print('Training the voted perceptron on classes: ', class1, 'and: ', class2, 'in dimension = ', dimension)
+    partial = kernel_voted_perceptron.train(X_train, y_train)
 
-        kernel_voted_perceptron.train(X_train, Y_train)
+    errors_during_training.append(partial)        #we save the number of errors in that dimension for every tenth of an epoch
 
-        print('Training completed')
-        errors.append(kernel_voted_perceptron.errors)
+    print('Training completed')
 
-        predictions = kernel_voted_perceptron.vote(X_test)
+    predictions = kernel_voted_perceptron.vote(X_test, partial)
 
-        print('Accuracy score of the prediction on test labels in dimension', dimension, ':', accuracy_score(Y_test, predictions))
-        print('Classification report for prediction in dimension', dimension, ': \n\n', classification_report(Y_test, predictions))
-        print()
+    #we calculate the number of mistaken labels on the test set for every tenth of an epoch and we normalize it to the total number of test instances
+    for i in range(len(predictions[0])):
+        error = 0
+        for j in range(len(predictions)):
+            if predictions[j][i] != y_test[j]: error += 1
+        errors.append(error * 100/ len(predictions))
 
-    #plotting the errors-per-epoch function
+    #this vector represent the prediction using the final version of the weight vector
+    final_prediction = [row[-1] for row in predictions]
+
+    print('Accuracy score of vote method on test labels in dimension', dimension, ':', accuracy_score(y_test, final_prediction) * 100, '%')
+    print('Classification report for prediction in dimension', dimension, ': \n\n', classification_report(y_test, final_prediction))
+    print()
+
+    #plot the test error for this dimension
     plt.rcParams['font.size'] = 8
-    plt.title('Errors during training on classes ' + str(class1) + ' and ' + str(class2))
+    plt.title('Test Error in d = ' + str(dimension) + ' on classes ' + str(class1) + ' and ' + str(class2))
     plt.xlabel('Epochs')
-    plt.ylabel('Errors')
-    plt.plot(plot_epochs, errors[0], label = 'dimension = 1')
-    plt.plot(plot_epochs, errors[1], label = 'dimension = 2')
-    plt.plot(plot_epochs, errors[2], label = 'dimension = 3')
-    plt.legend()
+    plt.ylabel('Test Error')
+    plt.plot(plot_epochs, errors, label = 'dimension = 1')
     plt.show()
+
+#plot the learning curve for the 3 dimensions
+plt.rcParams['font.size'] = 8
+plt.title('Learning rate in training on classes ' + str(class1) + ' and ' + str(class2))
+plt.xlabel('Epochs')
+plt.ylabel('Errors')
+plt.plot(plot_epochs, errors_during_training[0], label = 'dimension = 1')
+plt.plot(plot_epochs, errors_during_training[1], label = 'dimension = 2')
+plt.plot(plot_epochs, errors_during_training[2], label = 'dimension = 3')
+plt.legend()
+plt.show()
